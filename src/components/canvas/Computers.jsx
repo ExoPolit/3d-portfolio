2446d9ebb9716968a0a352a011e8a66a5f09e867
ExoPolit <exopolit@gmx.de>
2024-02-21 18:04:07 +0100

    fixed responsive Computer bug

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -34,7 +34,7 @@ const [isMobile, setIsMobile] = useState(false);
 
 useEffect(() => {
   // Add a listener for changes to the screen size
-const mediaQuery = window.matchMedia(" (max-width: 550px) ")
+const mediaQuery = window.matchMedia(" (max-width: 500px) ")
 
 // Set the initial value of the 'isMobile' state variable  
 setIsMobile(mediaQuery.matches);
